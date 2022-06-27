9ef8d0c29e221fd11d2dc279a92882747c09be14
unknown <nagasaimanoj333@gmail.com>
2022-06-27 11:44:13 +0530

    [Manoj]Adding_Employee_attendence

---
+++ employeewage.php
@@ -0,0 +1,14 @@
+<?php
+echo "WELCOME TO EMPLOYEE WAGE PROGRAM \n";
+$isPresent=1;
+$random_number=rand(0,1);
+//echo "$random_number \t";
+if($random_number==$isPresent)
+{
+    echo "EMPLOYEE IS PRESENT \n" ;
+}
+else
+{
+    echo "EMPLOYEE IS ABSENT \n";
+}
+?>
