aa6f817d20528df14519c0559e19a30824b1d0ab
unknown <nagasaimanoj333@gmail.com>
2022-06-28 06:57:45 +0530

    [MAnoj]Adding_Swtch_Case

--- employeewage.php
+++ employeewage.php
@@ -6,19 +6,18 @@ $Employee_Wage_PerHour=20;
 $empHrs=0;
 $random_number=rand(0,2);
 //echo "$random_number \t";
-if($random_number==$isPresent)
-{
-    echo "EMPLOYEE IS PRESENT WORK FOR FULL TIME \n" ;
-    $empHrs=8;
-}
-elseif($random_number==$isAbsent)
-{
-    echo "EMPLOYEE IS PRESENT AND WORK FOR PART TIME \n";
-    $empHrs=4;
-}
-else
-{
-    echo "EMPLOYEE IS ABSENT \n";
+switch($random_number){
+    case 1:
+        echo "EMPLOYEE IS PRESENT WORK FOR FULL TIME \n" ;
+        $empHrs=8;
+    break;
+    case 2:
+        echo "EMPLOYEE IS PRESENT AND WORK FOR PART TIME \n";
+         $empHrs=4;
+    break;
+    default:
+        echo "EMPLOYEE IS ABSENT \n";
+    break;
 }
 $empWage=$Employee_Wage_PerHour*$empHrs;
 echo "$empWage";
