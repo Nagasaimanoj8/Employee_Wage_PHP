897ccb5de7f132c30b75aabbc099e3679d1dd61c
unknown <nagasaimanoj333@gmail.com>
2022-06-28 07:40:04 +0530

    [Manoj]Adding_Wages_For_Month

--- employeewage.php
+++ employeewage.php
@@ -2,15 +2,17 @@
 echo "WELCOME TO EMPLOYEE WAGE PROGRAM \n";
 $isPresent=1;
 $isAbsent=2;
+$Emoloyee_Working_Hours=100;
 $Employee_Wage_PerHour=20;
 $Empoyee_Working_Days=20;
 $empHrs=0;
 $totalWage=0;
 $random_number=rand(0,2);
-$day=1;
+$Employee_day=1;
 //echo "$random_number \t";
-for($day=1;$day<=$Empoyee_Working_Days;$day++)
+while($Employee_day<=$Empoyee_Working_Days && $empHrs<=$Emoloyee_Working_Hours)
 {
+
 switch($random_number){
     case 1:
         echo "EMPLOYEE IS PRESENT WORK FOR FULL TIME \n" ;
@@ -24,9 +26,11 @@ switch($random_number){
         echo "EMPLOYEE IS ABSENT \n";
     break;
 }
+$Employee_day++;
 $empWage=$Employee_Wage_PerHour*$empHrs;
 echo "$empWage \n";
 $totalWage+=$empWage;
 }
+
 echo "Wages For a Month is:"."$totalWage \n";
 ?>
