2efd2641db2b269fd7c8e1489c19c3b78c14ea56
unknown <nagasaimanoj333@gmail.com>
2022-06-28 06:30:48 +0530

    [Manoj]Adding_Daily_Employee_Wage

--- employeewage.php
+++ employeewage.php
@@ -1,14 +1,19 @@
 <?php
 echo "WELCOME TO EMPLOYEE WAGE PROGRAM \n";
 $isPresent=1;
+$Employee_Wage_PerHour=20;
+$empHrs=0;
 $random_number=rand(0,1);
 //echo "$random_number \t";
 if($random_number==$isPresent)
 {
     echo "EMPLOYEE IS PRESENT \n" ;
+    $empHrs=8;
 }
 else
 {
     echo "EMPLOYEE IS ABSENT \n";
 }
+$empWage=$Employee_Wage_PerHour*$empHrs;
+echo "$empWage";
 ?>
