34fd8be755705205a71abe1b18d83753641e4991
unknown <nagasaimanoj333@gmail.com>
2022-06-28 06:51:45 +0530

    [Manoj]Adding_Part_Time

--- employeewage.php
+++ employeewage.php
@@ -1,14 +1,20 @@
 <?php
 echo "WELCOME TO EMPLOYEE WAGE PROGRAM \n";
 $isPresent=1;
+$isAbsent=2;
 $Employee_Wage_PerHour=20;
 $empHrs=0;
-$random_number=rand(0,1);
+$random_number=rand(0,2);
 //echo "$random_number \t";
 if($random_number==$isPresent)
 {
-    echo "EMPLOYEE IS PRESENT \n" ;
+    echo "EMPLOYEE IS PRESENT WORK FOR FULL TIME \n" ;
     $empHrs=8;
+}
+elseif($random_number==$isAbsent)
+{
+    echo "EMPLOYEE IS PRESENT AND WORK FOR PART TIME \n";
+    $empHrs=4;
 }
 else
 {
