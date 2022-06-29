e514e63c22063870c4365069d7c29075372ce574
unknown <nagasaimanoj333@gmail.com>
2022-06-29 07:00:52 +0530

    [Manoj]Added:Class_Method_ToCompute_EMpWage

--- employeewage.php
+++ employeewage.php
@@ -1,14 +1,17 @@
 <?php
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM \n";
-$Employee_Wage_PerHour=20;
-$MAX_Employee_Working_Days_in_Month=20;
-$empHrs;
-$total_emp_Wage_Per_Month=0;
-$Num_of_Working_day=1;
+class Employeewage{
+public $Max_Emoloyee_Working_Hours=100;
+public $Employee_Wage_PerHour=20;
+public $MAX_Employee_Working_Days_in_Month=20;
+public $total_Emp_WorkingHrs=0;
+public $total_emp_Wage_Per_Month=0;
+public $Num_of_Working_day=1;
+public $empHrs;
 //echo "$random_number \t";
-while($Num_of_Working_day<=$MAX_Employee_Working_Days_in_Month)
+public function claculatemonthlywage(){
+while($this->Num_of_Working_day<=$this->MAX_Employee_Working_Days_in_Month && $this->total_Emp_WorkingHrs<=$this->Max_Emoloyee_Working_Hours)
 {
-$random_number=rand(0,2);
+    $random_number=rand(0,2);
 switch($random_number){
     case 1:
         echo "EMPLOYEE IS PRESENT WORK FOR FULL TIME \n" ;
@@ -20,12 +23,18 @@ switch($random_number){
     break;
     default:
         echo "EMPLOYEE IS ABSENT \n";
+        $empHrs=0;
     break;
 }
-$Num_of_Working_day++;
-$daily_empWage=$Employee_Wage_PerHour*$empHrs;
+$this->Num_of_Working_day++;
+$this->total_Emp_WorkingHrs+=$empHrs;
+$daily_empWage=$this->Employee_Wage_PerHour*$empHrs;
 echo "$daily_empWage \n";
-$total_emp_Wage_Per_Month+=$daily_empWage;
+$this->total_emp_Wage_Per_Month+=$daily_empWage;
 }
-echo "Wages For a Month is:"."$total_emp_Wage_Per_Month \n";
+echo "Wages For a Month is:"."$this->total_emp_Wage_Per_Month \n";
+}
+}
+$empwage=new Employeewage();
+$empwage->claculatemonthlywage();
 ?>
